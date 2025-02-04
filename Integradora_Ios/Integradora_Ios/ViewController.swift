9de7637ea28a38514d41b4fa619e5120de801f8b
mac <mac@Mac-mini-de-mac.local>
2025-02-04 17:10:45 -0600

    first commit

---
+++ Integradora_Ios/Integradora_Ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Integradora_Ios
+//
+//  Created by mac on 04/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
