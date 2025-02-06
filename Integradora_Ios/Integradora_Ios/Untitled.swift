c5de5cb6f26326f05f169dea38e685408ae65c59
Tomasillo <tomasvillegasg18@gmail.com>
2025-02-06 09:48:22 -0600

    commit

---
+++ Integradora_Ios/Integradora_Ios/Untitled.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  Integradora_Ios
+//
+//  Created by mac on 05/02/25.
+//
+
+import UIKit
+
+class Untitled: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
