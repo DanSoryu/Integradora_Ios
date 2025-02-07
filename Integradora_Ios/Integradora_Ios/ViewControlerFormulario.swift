23d1704c9bdd2040f1cf5982512008ebed6f7f4e
TomasilloV <tomasvillegasg18@gmail.com>
2025-02-06 19:09:41 -0600

    Vistas Tomas

--- Integradora_Ios/Integradora_Ios/ViewControlerFormulario.swift
+++ Integradora_Ios/Integradora_Ios/ViewControlerFormulario.swift
@@ -1,31 +1,8 @@
-//
-//  ViewControlerFormulario.swift
-//  Integradora_Ios
-//
-//  Created by mac on 05/02/25.
-//
-
 import UIKit
 
-class ViewControlerFormulario: UIViewController {
-
-    @IBOutlet weak var checkboxButton: UIButton!
-
-        var isChecked = false
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            updateCheckbox()
-        }
-
-        @IBAction func checkboxTapped(_ sender: UIButton) {
-            isChecked.toggle()
-            updateCheckbox()
-        }
-
-        func updateCheckbox() {
-            let imageName = isChecked ? "checkmark.square.fill" : "square"
-            checkboxButton.setImage(UIImage(systemName: imageName), for: .normal)
-        }
-
+class ViewControllerFormulario: UIViewController{
+       
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
