a10418e0744e04f2132952acaf540127c15b5def
TomasilloV <tomasvillegasg18@gmail.com>
2025-02-07 18:29:34 -0600

    Vistas Tomas

---
+++ Integradora_Ios/Integradora_Ios/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  Integradora_Ios
+//
+//  Created by mac32 on 07/02/25.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imvEstado: UIImageView!
+    @IBOutlet weak var lblFecha: UILabel!
+    @IBOutlet weak var lblDescripcion: UILabel!
+    @IBOutlet weak var lblEstado: UILabel!
+}
