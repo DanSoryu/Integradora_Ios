44a65232f785a79e66e2df39be86c354f97ff479
TomasilloV <tomasvillegasg18@gmail.com>
2025-02-11 18:18:42 -0600

    cambios finales

--- Integradora_Ios/Integradora_Ios/ViewControlerFormulario.swift
+++ Integradora_Ios/Integradora_Ios/ViewControlerFormulario.swift
@@ -1,8 +1,7 @@
 import UIKit
 
 class ViewControllerFormulario: UIViewController{
-       
-    @IBOutlet weak var fghgf: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
